f66472186aa877552535a0e697421280b9016012
nkchambers <nkchames@gmail.com>
2023-06-01 14:54:43 -0700

    adding JS_CC_introduction for 6-1-23

---
+++ introduction.js
@@ -0,0 +1,10 @@
+//CODECADEMY JAVASCRIPT CODING CHALLENGES - introductions challenge 
+
+// Task 1 - Write a function, greetWorld(). 
+// Your function should have no parameters and return the string 'Hello, World!'.
+const greetWorld = () => {
+    return 'Hello, World!';
+}
+
+console.log(greetWorld());
+
